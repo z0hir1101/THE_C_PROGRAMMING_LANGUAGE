6d6e6461238646ae7d316e12d291d4c413a2a79c
z0hir1101 <z0hir1011.b@gmail.com>
2024-12-31 11:13:16 +0300

    new 5.8-arrays.c

---
+++ 5.8-arrays.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+static char daytab[2][13] = {
+    {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31},
+    {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}
+};
+
+int main()
+{
+    int month, day, day_of_year(int, int, int);
+    void month_day(int, int, int *, int *);
+    month_day(1980, 373, &month, &day);
+
+    printf("%d\n", day_of_year(2024, 14, 32));
+    printf("%d.%d\n", month, day); 
+    
+}
+
+int day_of_year(int year, int month, int day)
+{
+    int i, leap;
+
+    leap = year%4 == 0 && year%100 != 0 || year%400 == 0;
+    if (year < 0 || month > 12 || daytab[leap][month])
+        return -1;
+    for (i = 1; i < month; i++)
+        day += daytab[leap][i];
+    return day;
+}
+
+void month_day(int year, int yearday, int *pmonth, int *pday)
+{
+    int i, leap;
+
+    leap = year%4 == 0 && year%100 != 0 || year%400 == 0;
+    if (year < 0 || yearday > ((leap) ? 366 : 365))
+        return ;
+    for (i = 1; yearday > daytab[leap][i]; i++)
+        yearday -= daytab[leap][i];
+    *pmonth = i;
+    *pday = yearday;
+}
