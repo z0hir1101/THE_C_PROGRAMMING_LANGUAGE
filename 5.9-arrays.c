e63593635b16e05c3a639bfad0119f75f5ba2079
z0hir1101 <z0hir1011.b@gmail.com>
2025-01-01 00:42:01 +0300

    new 5.9-arrays.c

--- 5.9-arrays.c
+++ 5.9-arrays.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
-static char daytab[2][13] = {
-   {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31},
-   {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}
+static char *daytab[] = {
+   (char []){0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31},
+   (char []){0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}
 };
 
 int main()
@@ -11,7 +11,7 @@ int main()
    void month_day(int, int, int *, int *);
    month_day(1980, 343, &month, &day);
 
-   printf("%d\n", day_of_year(2024, 14, 32));
+   printf("%d\n", day_of_year(2024, 51, 20));
    printf("%d.%d\n", month, day); 
 
 }
@@ -21,22 +21,24 @@ int day_of_year(int year, int month, int day)
    int i, leap;
 
    leap = year%4 == 0 && year%100 != 0 || year%400 == 0;
-   if (year < 0 || month > 12 || day > daytab[leap][month])
+   if (year < 0 || month > 12 || day > *(*(daytab+leap)+month))
       return -1;
-   for (i = 1; i < month; i++)
-      day += daytab[leap][i];
+   while (month-- > 0)
+      day += *daytab[leap]++;
    return day;
 }
 
 void month_day(int year, int yearday, int *pmonth, int *pday)
 {
-   int i, leap;
+   int leap;
+   char *p;
 
    leap = year%4 == 0 && year%100 != 0 || year%400 == 0;
    if (year < 0 || yearday > ((leap) ? 366 : 365))
       return ;
-   for (i = 1; yearday > daytab[leap][i]; i++)
-      yearday -= daytab[leap][i];
-   *pmonth = i;
+   p = daytab[leap];
+   while (yearday > *p)
+      yearday -= *p++;
+   *pmonth = p - daytab[leap];
    *pday = yearday;
 }
